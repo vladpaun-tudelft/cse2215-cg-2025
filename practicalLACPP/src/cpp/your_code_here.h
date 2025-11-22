838a32a94cfa0f22fc3f4bcea980fef6627845e7
vladpaun1 <vlad.paun06@gmail.com>
2025-11-22 22:36:30 +0100

    first practical cpp part

--- practicalLACPP/src/cpp/your_code_here.h
+++ practicalLACPP/src/cpp/your_code_here.h
@@ -12,8 +12,19 @@
 // ==================================
 std::pair<float, float> statistics(std::span<const float> values)
 {
-    // Your solution goes here
-    return { 123.0f, 456.0f };
+    if (values.empty()) return {0.0f, 0.0f};
+    float mean = 0.0;
+    float sd = 0.0;
+    for (float v : values) {
+        mean += v;
+    }
+    mean /= (float)values.size();
+
+    for (float v : values) {
+        sd += (v - mean) * (v - mean);
+    }
+    sd = std::sqrt(sd / (float) values.size());
+    return { mean, sd };
 }
 
 // ==================================
@@ -23,10 +34,20 @@ struct Tree {
     float value;
     std::vector<Tree> children;
 };
+float countTreeRecursive(const Tree& tree, bool countOnlyEvenLevels, bool oddLevel)
+{
+    if (tree.children.empty())
+        return countOnlyEvenLevels && oddLevel ? 0.0f : tree.value;
+
+    float sum = countOnlyEvenLevels && oddLevel ? 0.0f : tree.value;
+    for (const Tree& child : tree.children) {
+        sum += countTreeRecursive(child, countOnlyEvenLevels, !oddLevel);
+    }
+    return sum;
+}
 // Your implementation goes here. Feel free to define helper functions/structs/classes in this
 // file if you need to. Make sure to put them above this function to prevent linker warnings.
 float countTree(const Tree& tree, bool countOnlyEvenLevels)
 {
-    // Your solution goes here
-    return 0.0f;
+    return countTreeRecursive(tree, countOnlyEvenLevels, false);
 }
